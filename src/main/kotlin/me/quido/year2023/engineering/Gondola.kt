e7b42f7b5fe6d37c7120fa87319e8896d48b0e4a
Quido Meijer <quidome@gmail.com>
2023-12-10 22:34:36 +0100

    Revert "Put the engine data classes in separate file"
    This reverts commit 45cc5b891e2b88dec71cf26bf0adf072246ef3d7.

--- src/main/kotlin/me/quido/year2023/engineering/Gondola.kt
+++ src/main/kotlin/me/quido/year2023/engineering/Gondola.kt
@@ -11,6 +11,34 @@ data class Coordinate(
     val column: Int,
 )
 
+data class EnginePart(
+    val coordinate: Coordinate,
+    val type: Char,
+    val partNumbers: List<Int>,
+) {
+    fun isGear() = type == '*' && partNumbers.size == 2
+}
+
+data class PartNumber(
+    val coordinate: Coordinate,
+    val number: Int,
+) {
+    fun value() = number
+
+    fun coordinatesReach(gridLimits: Coordinate): List<Coordinate> =
+        this.growBoundedRange(coordinate.row..coordinate.row, gridLimits.row)
+            .flatMap { row ->
+                this.growBoundedRange(this.range(), gridLimits.row).map { column ->
+                    Coordinate(row, column)
+                }
+            }
+
+    private fun range() = coordinate.column..coordinate.column + number.toString().lastIndex
+
+    private fun growBoundedRange(range: IntRange, max: Int) =
+        (range.first - 1).coerceAtLeast(0)..(range.last + 1).coerceAtMost(max)
+}
+
 class Gondola : Solver() {
     override fun solve(input: List<String>): Pair<Any, Any> {
         val engineParts = engineParts(input.nonBlank())
