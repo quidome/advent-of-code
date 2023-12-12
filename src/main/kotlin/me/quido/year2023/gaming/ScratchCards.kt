2f992609891c43c11a06664494cdf394ceed545b
Quido Meijer <quidome@gmail.com>
2023-12-13 00:14:00 +0100

    Testing some ideas for part 2
    totally not functional but have to push

--- src/main/kotlin/me/quido/year2023/gaming/ScratchCards.kt
+++ src/main/kotlin/me/quido/year2023/gaming/ScratchCards.kt
@@ -3,13 +3,29 @@ package me.quido.year2023.gaming
 import me.quido.util.Solver
 import me.quido.util.nonBlank
 
+data class Card(
+    val id: Int,
+    val amount: Int,
+    val winningNumbers: List<Int>,
+    val cardNumbers: List<Int>,
+)
+
 class ScratchCards : Solver() {
     override fun solve(input: List<String>): Pair<Any, Any> {
         val numbers = input.nonBlank()
             .map { extractNumbers(it) }
             .map { (winning, ours) -> winning.intersect(ours.toSet())}
 
+        val cardStack = input.nonBlank()
+            .map {readCard(it)}
+
+        println(cardStack)
+
         return numbers.sumOf { power(2, it.size - 1) } to 0
+    }
+
+    private fun readCard(card: String): Card {
+        return Card(0,0,listOf(), listOf())
     }
 
     private fun extractNumbers(card: String): List<List<Int>> {
