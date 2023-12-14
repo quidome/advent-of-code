cfbe032eebc96d19c92dae7a6531fa8e99773bd7
Quido Meijer <quidome@gmail.com>
2023-12-14 10:25:20 +0100

    Adds completed day 4
    And did a little bit of refactoring.

--- src/main/kotlin/me/quido/year2023/gaming/ScratchCards.kt
+++ src/main/kotlin/me/quido/year2023/gaming/ScratchCards.kt
@@ -11,30 +11,31 @@ data class Card(
 ) {
     fun matchingNumbers() = winningNumbers.intersect(cardNumbers.toSet())
     fun wins() = matchingNumbers().size
-    fun increaseAmount() { amount++ }
+    fun increaseAmount(increase: Int = 1) {
+        amount += increase
+    }
 }
 
 class ScratchCards : Solver() {
     override fun solve(input: List<String>): Pair<Any, Any> {
         val cardStack = input.nonBlank()
-            .map {readCard(it)}
+            .map { readCard(it) }
 
         val scratchCardPoints = cardStack
             .map { it.matchingNumbers() }
             .map { power(2, it.size - 1) }
+            .sum()
 
-        /* TODO: find a way to keep track of number of cards
-            My initial idea was to keep a counter per card, and just increase that for the following cards
-            I thought the code below was going to work. There are mistakes in the offsets, or I cannot do what I do.
-         */
-        println(cardStack.mapIndexed{ index, it ->
-            for (i in index + 1 ..  index + it.wins().coerceAtMost(cardStack.lastIndex)) {
-                cardStack[i].increaseAmount()
-            }
-            it.amount}
-        )
+        val finalCardStackSize = cardStack
+            .mapIndexed { index, card ->
+                val cardRange = index + 1..(index + card.wins()).coerceAtMost(cardStack.lastIndex)
+                for (i in cardRange) {
+                    cardStack[i].increaseAmount(card.amount)
+                }
+                card.amount
+            }.sum()
 
-        return scratchCardPoints.sum() to 30
+        return scratchCardPoints to finalCardStackSize
     }
 
     private fun readCard(card: String): Card {
@@ -54,10 +55,10 @@ class ScratchCards : Solver() {
         .map { it.toInt() }
 
     private fun power(baseVal: Int, exponentVal: Int): Int {
-        return if (exponentVal < 0 ) {
+        return if (exponentVal < 0) {
             0
         } else if (exponentVal != 0) {
-            baseVal  * power(baseVal, exponentVal - 1)
+            baseVal * power(baseVal, exponentVal - 1)
         } else {
             1
         }
