e486476ad8a249eb35c4b8ab9645f10d3aa2a0c0
Quido Meijer <quidome@gmail.com>
2023-12-24 00:56:32 +0100

    Day 6 completed

---
+++ src/main/kotlin/me/quido/year2023/gaming/BoatRaceCalculator.kt
@@ -0,0 +1,47 @@
+package me.quido.year2023.gaming
+
+import me.quido.util.Solver
+import me.quido.util.nonBlank
+import me.quido.util.toLongList
+
+data class Race(
+    val duration: Long,
+    val winningDistance: Long,
+) {
+    companion object {
+        fun fromPair(raceData: Pair<Long, Long>): Race =
+            Race(duration = raceData.first, winningDistance = raceData.second)
+    }
+
+    fun winningDurations(): List<Long> = (0..duration).map { distanceTraveled(it) }.filter { it > winningDistance }
+
+    private fun distanceTraveled(time: Long) = time * (duration - time)
+}
+
+class BoatRaceCalculator : Solver() {
+    override fun solve(input: List<String>): Pair<Any, Any> {
+        val raceData = input.nonBlank().map { it.substringAfter(':').toLongList() }
+
+        val races = listsToListOfPairs(raceData.first(), raceData.last()).map { Race.fromPair(it) }
+        val winningDurationsAmount = races.map { it.winningDurations().size }
+
+        val correctedRaceData = input.nonBlank().map {
+            it.substringAfter(':').filter {
+                it.isDigit()
+            }.toLong()
+        }
+        val race = Race.fromPair(correctedRaceData.first() to correctedRaceData.last())
+
+        return winningDurationsAmount.reduce { acc, ints -> acc * ints } to race.winningDurations().size
+    }
+}
+
+private fun listsToListOfPairs(keys: List<Long>, values: List<Long>): List<Pair<Long, Long>> {
+    if (keys.size != values.size) return emptyList()
+
+    val returnValue: MutableList<Pair<Long, Long>> = mutableListOf()
+    for (i in 0..keys.lastIndex) {
+        returnValue += keys[i] to values[i]
+    }
+    return returnValue
+}
---
+++ src/test/kotlin/me/quido/year2023/gaming/BoatRaceCalculatorTest.kt
@@ -0,0 +1,21 @@
+package me.quido.year2023.gaming
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class BoatRaceCalculatorTest {
+    private val boatRaceCalculator = BoatRaceCalculator()
+
+    @Test
+    fun `when given race data, calculate possible race outcomes`() {
+        val input = """
+            Time:      7  15   30
+            Distance:  9  40  200
+        """.trimIndent().lines()
+        val expected = 288 to 71503
+
+        val output = boatRaceCalculator.solve(input)
+
+        assertEquals(expected, output)
+    }
+}
