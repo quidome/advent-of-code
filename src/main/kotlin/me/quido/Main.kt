ec19018606bfc43fd0a66d8b55fb6d3c3803551a
Quido Meijer <quidome@gmail.com>
2024-12-02 00:02:24 +0100

    Disable 2023 to speed things up

--- src/main/kotlin/me/quido/Main.kt
+++ src/main/kotlin/me/quido/Main.kt
@@ -2,25 +2,19 @@ package me.quido
 
 import me.quido.util.Solver
 import me.quido.util.readFile
-import me.quido.year2023.engineering.EngineReader
-import me.quido.year2023.engineering.TrebuchetCalibrator
-import me.quido.year2023.farming.AlmanacReader
-import me.quido.year2023.gaming.BoatRaceCalculator
-import me.quido.year2023.gaming.CubeCalculator
-import me.quido.year2023.gaming.ScratchCardsCalculator
 import me.quido.year2024.planning.DistanceFinder
 import java.time.OffsetDateTime
 import java.time.temporal.ChronoUnit.MILLIS
 
-private val solvers2023: List<Solver> =
-    listOf(
-        TrebuchetCalibrator(),
-        CubeCalculator(),
-        EngineReader(),
-        ScratchCardsCalculator(),
-        AlmanacReader(),
-        BoatRaceCalculator(),
-    )
+// private val solvers2023: List<Solver> =
+//    listOf(
+//        TrebuchetCalibrator(),
+//        CubeCalculator(),
+//        EngineReader(),
+//        ScratchCardsCalculator(),
+//        AlmanacReader(),
+//        BoatRaceCalculator(),
+//    )
 
 private val solvers2024: List<Solver> =
     listOf(
@@ -28,7 +22,7 @@ private val solvers2024: List<Solver> =
     )
 
 fun main() {
-    2023.printSolutions(solvers2023)
+//    2023.printSolutions(solvers2023)
     2024.printSolutions(solvers2024)
 }
 
