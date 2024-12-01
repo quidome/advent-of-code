c3f923a58939dcd170480ff20c522b478808e76c
Quido Meijer <quidome@gmail.com>
2024-12-01 23:34:07 +0100

    Add working solution for day 1

--- src/main/kotlin/me/quido/year2024/planning/DistanceFinder.kt
+++ src/main/kotlin/me/quido/year2024/planning/DistanceFinder.kt
@@ -1,7 +1,32 @@
 package me.quido.year2024.planning
 
 import me.quido.util.Solver
+import kotlin.math.abs
 
 class DistanceFinder : Solver() {
-    override fun solve(input: List<String>): Pair<Any, Any> = true to true
+    override fun solve(input: List<String>): Pair<Any, Any> {
+        val (left, right) =
+            input
+                .map {
+                    it.split("""\D+""".toRegex()).let {
+                        Pair(it[0].toLong(), it[1].toLong())
+                    }
+                }.unzip()
+
+        val distanceSum =
+            left
+                .sorted()
+                .zip(right.sorted())
+                .map { (left, right) -> abs(left - right) }
+                .sum()
+
+        val frequencies: Map<Long, Int> = right.groupingBy { it }.eachCount()
+        val similarities =
+            left
+                .map {
+                    it * frequencies.getOrDefault(it, 0)
+                }.sum()
+
+        return distanceSum to similarities
+    }
 }
--- src/test/kotlin/me/quido/year2024/planning/DistanceFinderTest.kt
+++ src/test/kotlin/me/quido/year2024/planning/DistanceFinderTest.kt
@@ -4,19 +4,20 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class DistanceFinderTest {
+    val input =
+        """
+        3   4
+        4   3
+        2   5
+        1   3
+        3   9
+        3   3
+        """.trimIndent().lines()
+
     @Test
     fun `with given input, the correct distance should be returned`() {
         val distanceFinder = DistanceFinder()
-        val input =
-            """
-            3   4
-            4   3
-            2   5
-            1   3
-            3   9
-            3   3
-            """.trimIndent().lines()
-        val expected = 11 to false
+        val expected = 11L to 31L
 
         val output = distanceFinder.solve(input)
 
