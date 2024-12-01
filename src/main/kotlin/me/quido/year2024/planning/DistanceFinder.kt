8ba0a2ec73b5b2946dca35edb59ec13a7f578838
Quido Meijer <quidome@gmail.com>
2024-12-01 23:51:54 +0100

    Small improvements on solution

--- src/main/kotlin/me/quido/year2024/planning/DistanceFinder.kt
+++ src/main/kotlin/me/quido/year2024/planning/DistanceFinder.kt
@@ -7,26 +7,25 @@ class DistanceFinder : Solver() {
     override fun solve(input: List<String>): Pair<Any, Any> {
         val (left, right) =
             input
-                .map {
-                    it.split("""\D+""".toRegex()).let {
+                .map { line ->
+                    line.split("""\D+""".toRegex()).let {
                         Pair(it[0].toLong(), it[1].toLong())
                     }
                 }.unzip()
 
-        val distanceSum =
+        val distances =
             left
                 .sorted()
                 .zip(right.sorted())
                 .map { (left, right) -> abs(left - right) }
-                .sum()
 
-        val frequencies: Map<Long, Int> = right.groupingBy { it }.eachCount()
+        val frequencyMap: Map<Long, Int> = right.groupingBy { it }.eachCount()
         val similarities =
             left
                 .map {
-                    it * frequencies.getOrDefault(it, 0)
-                }.sum()
+                    it * frequencyMap.getOrDefault(it, 0)
+                }
 
-        return distanceSum to similarities
+        return distances.sum() to similarities.sum()
     }
 }
