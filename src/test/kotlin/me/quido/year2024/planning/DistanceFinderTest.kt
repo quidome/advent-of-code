6a1d7f7c6693f2b72e72949787275456cb7784fa
Quido Meijer <quidome@gmail.com>
2024-12-01 23:59:15 +0100

    Satisfy ktlint

--- src/test/kotlin/me/quido/year2024/planning/DistanceFinderTest.kt
+++ src/test/kotlin/me/quido/year2024/planning/DistanceFinderTest.kt
@@ -1,6 +1,6 @@
 package me.quido.year2024.planning
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class DistanceFinderTest {
